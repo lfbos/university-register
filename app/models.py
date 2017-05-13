b6b00bb7d921091b494ff922b12aa8f63444e6aa
Luis Boscan <luife642@gmail.com>
2017-05-13 16:05:42 -0300

    student model & migrations added

--- app/models.py
+++ app/models.py
@@ -6,3 +6,19 @@ class University(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=64, verbose_name='first name')
+    last_name = models.CharField(max_length=64, verbose_name='last name')
+    dni = models.CharField(max_length=64, verbose_name='student ID')
+    university = models.ForeignKey('University', related_name='students')
+
+    def __str__(self):
+        return self.get_full_name()
+
+    def get_full_name(self):
+        return "{} {}".format(
+            self.first_name,
+            self.last_name
+        )
