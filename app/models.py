3570e428f41924d071c5327e1e36920763fa5839
Luis Boscan <luife642@gmail.com>
2017-05-13 16:01:25 -0300

    University model added

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class University(models.Model):
+    name = models.CharField(max_length=128, verbose_name='university')
+
+    def __str__(self):
+        return self.name
